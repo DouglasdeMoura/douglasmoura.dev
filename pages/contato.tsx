6aef77cea9c15bdaf28491d97d111f1d2d070f04
Douglas Moura <douglas.ademoura@gmail.com>
2021-01-18 19:58:10 -0300

    chore: replace Formik in favor of React Hooks Form

--- pages/contato.tsx
+++ pages/contato.tsx
@@ -4,47 +4,9 @@ import Layout from '../components/layout'
 import { SITE_NAME } from '../lib/constants'
 import Header from '../components/header'
 import markdownStyles from '../styles/markdown-styles.module.css'
-import { Formik, Field, Form } from 'formik'
-import * as Yup from 'yup'
-import axios from 'axios'
-import { useState } from 'react'
-
-type ContactForm = {
-  email: string
-  message: string
-}
+import ContactForm from '../components/contact-form'
 
 const Contato = () => {
-  const [serverState, setServerState] = useState({ ok: false, msg: '' })
-
-  const handleServerResponse = (ok: boolean, msg: string) => {
-    setServerState({ ok, msg })
-  }
-
-  const formSchema = Yup.object().shape({
-    email: Yup.string()
-      .email('E-mail inválido')
-      .required('Obrigatório'),
-    message: Yup.string().required('Obrigatório')
-  })
-
-  const handleOnSubmit = (values: ContactForm, actions: any) => {
-    axios({
-      method: 'POST',
-      url: 'https://formspree.io/f/mwkwyelw',
-      data: values
-    })
-      .then(_ => {
-        actions.setSubmitting(false)
-        actions.resetForm()
-        handleServerResponse(true, 'Obrigado por sua mensagem!')
-      })
-      .catch(error => {
-        actions.setSubmitting(false)
-        handleServerResponse(false, error.response.data.error)
-      })
-  }
-
   return (
     <>
       <Head>
@@ -60,47 +22,7 @@ const Contato = () => {
               </h1>
               <div className="entry-content">
                 <div className={markdownStyles.markdown}>
-                  <Formik
-                    initialValues={{
-                      email: '',
-                      message: ''
-                    }}
-                    onSubmit={handleOnSubmit}
-                    validationSchema={formSchema}
-                  >
-                    {({ isSubmitting }) => (
-                      <Form className="flex flex-col">
-                        <label htmlFor="email" className="sr-only">E-mail</label>
-                        <Field
-                          id="email"
-                          name="email"
-                          type="email"
-                          placeholder="E-mail"
-                          className="field px-4 py-4 rounded-md mb-8"
-                          required
-                        />
-                        <label htmlFor="message" className="sr-only">Mensagem</label>
-                        <Field
-                          id="message"
-                          name="message"
-                          component="textarea"
-                          placeholder="Mensagem"
-                          className="field px-4 py-4 rounded-md mb-8"
-                          rows={6}
-                          required
-                        />
-                        <button type="submit" disabled={isSubmitting} className="px-4 py-2 rounded-md mb-8 font-bold bg-red-600 text-white">
-                          Enviar
-                        </button>
-
-                        {serverState && (
-                          <p className={!serverState.ok ? 'errorMsg' : ''}>
-                            {serverState.msg}
-                          </p>
-                        )}
-                      </Form>
-                    )}
-                  </Formik>
+                  <ContactForm />
                 </div>
               </div>
             </article>
