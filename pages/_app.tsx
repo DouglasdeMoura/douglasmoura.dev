2a363a6d5031df8807b8cbb678e50d911950e362
Douglas Moura <douglas.ademoura@gmail.com>
2021-02-18 09:06:12 -0300

    chore: add initial files and default document

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import 'modern-css-reset/dist/reset.min.css';
+import '../sass/globals.scss';
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body className="hfeed">
+          <div id="page" className="site">
+            <Main />
+            <NextScript />
+          </div>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument;
