ffe10d87bf04cb7dcce2e05add111d32316f2cfb
Douglas Moura <douglas.ademoura@gmail.com>
2021-03-09 13:30:05 -0300

    chore: add support to localization

---
+++ i18n/locales.ts
@@ -0,0 +1,59 @@
+import { useRouter } from 'next/router';
+
+// Key should always be in English
+const localization = {
+  'MMMM, LL y': {
+    'pt-BR': `LL 'de' MMMM 'de' y`,
+  },
+  'Skip to content': {
+    'pt-BR': 'Pular para o conteúdo',
+  },
+  'Home': {
+    'pt-BR': 'Início',
+  },
+  'About': {
+    'pt-BR': 'Sobre',
+  },
+  'Contact': {
+    'pt-BR': 'Contato',
+  },
+  'Hi, my name is <strong>Douglas Moura</strong>,<br /> Software Engineer, dad and amateur saxophonist.': {
+    'pt-BR': 'Olá, meu nome é <strong>Douglas Moura</strong>,<br /> engenheiro de software, pai e saxofonista nas horas vagas.'
+  },
+  'Last posts': {
+    'pt-BR': 'Últimos artigos',
+  },
+  'See all posts': {
+    'pt-BR': 'Ver todos os artigos',
+  },
+  'Read More': {
+    'pt-BR': 'Continue lendo',
+  },
+  '{0} minutes': {
+    'pt-BR': '{0} minutos',
+  },
+  '1 minute': {
+    'pt-BR': '1 minuto',
+  },
+  'Less than a minute': {
+    'pt-BR': 'Menos de um minuto',
+  },
+  'Posted on': {
+    'pt-BR': 'Publicado em',
+  },
+  'Updated at': {
+    'pt-BR': 'Atualizado em',
+  },
+  'Software Engineer': {
+    'pt-BR': 'Engenheiro de Software',
+  },
+};
+
+export default function __(text: string) {
+  const { locale } = useRouter();
+
+  if (locale === 'en')
+    return text;
+
+  return localization[text][locale];
+}
