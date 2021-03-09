31696f2f72663c092cfa2cac74417522e67d3315
Douglas Moura <douglas.ademoura@gmail.com>
2021-03-09 13:40:09 -0300

    chore: create default blog template

---
+++ components/template.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import { ReactNode } from 'react';
+import Footer from './footer';
+import Header from './header';
+
+interface IIndexProps {
+  children: ReactNode;
+  title: string;
+  description: string;
+  isHome?: boolean;
+}
+
+export default function Template({ children, title, description, isHome = false }: IIndexProps) {
+  const siteTitle = isHome ? title : description;
+
+  return (
+    <div id="page" className="site">
+      <Head>
+        <title>{title} | {description}</title>
+      </Head>
+      <Header />
+      <main id="primary" className="site-main">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
