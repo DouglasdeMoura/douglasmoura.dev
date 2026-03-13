fc55aed69fe062bcd9cf83bee1fa9678e6979412
Douglas Moura <douglas.ademoura@gmail.com>
2026-03-13 18:13:55 -0300

    chore: install Tailwind CSS

--- src/app/document.tsx
+++ src/app/document.tsx
@@ -1,3 +1,5 @@
+import styles from "./styles.css?url";
+
 export const Document: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => (
@@ -6,6 +8,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>@redwoodjs/starter-minimal</title>
+      <link rel="stylesheet" href={styles} />
       <link rel="modulepreload" href="/src/client.tsx" />
     </head>
     <body>
--- vite.config.mts
+++ vite.config.mts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import { redwood } from "rwsdk/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
+  environments: {
+    ssr: {},
+  },
   plugins: [
     cloudflare({
       viteEnvironment: { name: "worker" },
     }),
     redwood(),
+    tailwindcss(),
   ],
 });
