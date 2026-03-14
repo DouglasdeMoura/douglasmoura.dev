766d52879da9c9a5a0663210b0f2d0981046bb1f
Douglas Moura <douglas.ademoura@gmail.com>
2026-03-14 09:36:10 -0300

    refactor: replace @ alias with Node.js subpath imports (#)

--- src/worker.tsx
+++ src/worker.tsx
@@ -1,9 +1,9 @@
 import { render, route } from "rwsdk/router";
 import { defineApp } from "rwsdk/worker";
 
-import { Document } from "@/app/document";
-import { setCommonHeaders } from "@/app/headers";
-import { Home } from "@/app/pages/home";
+import { Document } from "#app/document.js";
+import { setCommonHeaders } from "#app/headers.js";
+import { Home } from "#app/pages/home.js";
 
 // biome-ignore lint/complexity/noBannedTypes: scaffold placeholder
 export type AppContext = Record<string, never>;
